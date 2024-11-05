f8d606ae62984922a3b33a7982230040f0535be5
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-11-05 15:46:16 +0900

    [Refactor]커서 기반 페이지네이션으로 개선 (#79)
    * chore : initData 추가
    
    * refactor : initData bulkquery로 진행
    
    * feat : 커서 기반 페이징 구현
    
    * mod : SecurityConfig의 /** 경로 접근 허용
    
    * mod : hasNext처리를 위한 로직 변경
    
    * feat : 멤버 페이징 리액트 구현
    
    * mod : testMember 생성 10만 -> 2천으로 수정
    
    * chore : initData 추가
    
    * refactor : initData bulkquery로 진행
    
    * feat : 커서 기반 페이징 구현
    
    * mod : SecurityConfig의 /** 경로 접근 허용
    
    * mod : hasNext처리를 위한 로직 변경
    
    * feat : 멤버 페이징 리액트 구현
    
    * mod : testMember 생성 10만 -> 2천으로 수정
    
    * chore : member config 파일 삭제 - Bean 충돌
    
    * del :  QuerydslConfig에 @EnableJpaAuditing 애노테이션 제거
    
    * del :  member 도메인에 있는 임시 order 삭제
    
    * del :  member 도메인에 있는 임시 Product 삭제

--- src/main/kotlin/edu/example/kotlindevelop/domain/member/controller/AdminController.kt
+++ src/main/kotlin/edu/example/kotlindevelop/domain/member/controller/AdminController.kt
@@ -6,10 +6,8 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/adm")
@@ -28,6 +26,23 @@ class AdminController (
 
       return ResponseEntity.ok(responseDto)
     }
+
+    //무한 스크롤 회원 정보 조회하기
+    @GetMapping("/members/all/cursor")
+    fun getMembersCursorBased(
+        @RequestParam lastCreatedAt : LocalDateTime?,
+        @RequestParam lastId : Long?,
+        @RequestParam (defaultValue = "10") limit: Int,
+    ): ResponseEntity<MemberDTO.MemberCursorResponse> {
+        val response : MemberDTO.MemberCursorResponse = memberService.readAllCursorBased(
+            lastCreatedAt,
+            lastId,
+            limit
+        )
+
+        return ResponseEntity.ok(response)
+    }
+
 
     // 아래는 ProductService merge 후 완성
 
